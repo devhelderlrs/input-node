907471c05a7c4219d240ecce922bc929b38c6104
devhelderlrs <helderl4004@gmail.com>
2022-07-12 22:34:48 -0300

    envio da solucao para a tividade de pegar input do console e printar os inputs de forma ordenada e separado por linhas

---
+++ app.js
@@ -0,0 +1,23 @@
+import input from 'input'; // IMPORT BIBLIOTECA INPUT P/ INTERACAO CONSOLE
+import chalk from 'chalk'; // IMPORT BIBLIOTECA PARA COLORIR MSGS DO CONSOLE
+
+async function pegaInput() { // FUNÇÃO ASSINCRONA
+
+    let arr = [];
+    let perguntaCss;
+
+    do {
+        perguntaCss ? arr.push(perguntaCss) : ''; // TERNÁRIO PARA PREVENÇÃO DO INPUT PRA SAÍDA DO TERMINAL NÃO SER INCLUSO NO ARRAY.
+
+        perguntaCss = (await input.text('Digite uma propriedade CSS: \n')).toLowerCase(); // MÉTODO DA BIBLIOTECA -INPUT- QUE FAZ A INTERAÇÃO NO CONSOLE.LOG
+
+    } while (perguntaCss != 'sair'); // ENQUANTO O INPUT FOR DIFERENTE DE 'SAIR' O LAÇO EXECUTA.
+
+    arr = arr.sort(); // .SORT() -> ORDENA O CONTEÚDO DO ARRAY EM ORDEM CRESCENTE
+
+    for(let i=0;i < arr.length; i++){ // LAÇO FOR PARA QUEBRAR LINHA NO CONSOLE
+        console.log(chalk.green(`Propriedades CSS: ${arr[i]}`));
+    }
+}
+
+pegaInput(); // CHAMADA DA FUNÇÃO
